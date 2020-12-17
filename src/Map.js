3e66d65d69fc6606f41ac6c0035955876227e713
Daniel Austi <danielausti@gmail.com>
2020-12-17 12:55:15 -0800

    update map.js
    changed leaflet map component to its default name of mapcontainer

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Map.css";
-import { MapContainer as LeafletMap, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 
+
+// change map center and zoom value when country is changed
 function ChangeMap({ center, zoom }) {
   const map = useMap();
   map.setView(center, zoom);
@@ -12,13 +14,13 @@ function ChangeMap({ center, zoom }) {
 function Map({ center, zoom }) {
   return (
     <div className="map">
-    <LeafletMap>
+    <MapContainer>
     <ChangeMap center={center} zoom={zoom}/>
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    </LeafletMap>
+    </MapContainer>
     </div>
   );
 }
